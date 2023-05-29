64915466183381a3f9bbff3ba46a7bd9f2d377fb
DavGuett <soulsec@protonmail.com>
2023-05-29 20:48:39 -0300

    Position class created

--- src/chess/Program.java
+++ src/chess/Program.java
@@ -1,9 +1,12 @@
 package chess;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Position posicao = new Position(3,5);
+		System.out.println(posicao);
 
 	}
 
